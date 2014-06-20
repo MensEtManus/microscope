a18ae37b7a931b2fc82786a88c37bf79f71f084b
Albert Yang <ayoungbest@gmail.com>
2014-06-20 00:34:53 -0400

    added server fixture and modified posts_list.js

--- client/views/posts/posts_list.js
+++ client/views/posts/posts_list.js
@@ -1,20 +1,5 @@
-var postData = [
-	{
-		title: 'Introducing Telescope',
-		author: 'Albert Yang',
-		url: 'http://alberthbyang.com'	
-	},
-	{
-		title: 'Meteor',
-		author: 'Tom Coleman',
-		url: 'http://meteor.com'
-	},
-	{
-		title: 'The Meteor Book',
-		author: 'Tome Coleman',
-		url: 'http://themeteorbook.com'
-	}
-];
 Template.postsList.helpers({
-	posts: postData
+	posts: function () {
+		return Posts.find();
+	}
 });

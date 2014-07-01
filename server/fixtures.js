efb493b5a9ecc92ff2739dad815826c21f2c11ff
Albert Yang <ayoungbest@gmail.com>
2014-07-01 19:35:56 -0400

    test

--- server/fixtures.js
+++ server/fixtures.js
@@ -1,6 +1,6 @@
 if (Posts.find().count() === 0) {
     Posts.insert({
-        title: 'Welcome to microscope',
+        title: 'A Hacker, an entrepreneur, a photographer',
         author: 'Albert',
         url: 'http://alberthbyang.com'
     });
